7e7362607f8f1cea927447fb688610bf4468f594
Decoroader <secbittree@gmail.com>
2021-11-13 20:06:21 +0200

    correct placing on the surface
    with crutches fixed the problem with incorrect placing of objects above the surface

--- ItemsOnSurface/OnSurface/Assets/Scripts/Ground3Comps/ItemPlacer.cs
+++ ItemsOnSurface/OnSurface/Assets/Scripts/Ground3Comps/ItemPlacer.cs
@@ -18,14 +18,15 @@ public class ItemPlacer : MonoBehaviour
     private List<Renderer> SurfaceChildrenRendererList = new List<Renderer>();
     private Bounds boundsOverallSurface;
 //#if UNITY_EDITOR
-    [SerializeField] private bool settingsSwitcher;
+    [SerializeField] private bool objectReplacing;
     private bool tempSwitcher;
 //#endif
     void Start()
     {
-        numberJam = 5;
+        numberJam = 9;
         maxObjectSize = 3;  
-        settingsSwitcher = tempSwitcher = true;
+        objectReplacing = tempSwitcher = true;
+
         GetSurfaceDataObjectsForPlace();
 
         PlacerFirst(numberJam);
@@ -33,9 +34,18 @@ public class ItemPlacer : MonoBehaviour
 #if UNITY_EDITOR
     private void Update()
     {
-        if (settingsSwitcher != tempSwitcher)
+        if (objectReplacing != tempSwitcher)
         {
-            tempSwitcher = settingsSwitcher;
+            tempSwitcher = objectReplacing;
+            
+            int tsh = 0;
+            foreach (GameObject curObj in ListObjectsForPlace)
+            {
+                curObj.transform.position = Vector3.up * (15 + tsh);
+                tsh += 2;
+            }
+            ListPlacedObjects.Clear();
+
             PlacerFirst(numberJam);
         }
     }
@@ -60,32 +70,30 @@ public class ItemPlacer : MonoBehaviour
 
     void PlacerFirst(int numbObj)
     {
-        Vector3 tempPos;
-        ListPlacedObjects.Clear();
-
         while (numbObj > 0)
         {
-            tempPos = GetCoordinate(boundsOverallSurface);
             RaycastHit hit;
-            if (Physics.Raycast(tempPos, Vector3.down, out hit, 11))
+
+            if (Physics.Raycast(GetCoordinate(boundsOverallSurface), Vector3.down, out hit))
             {
-                tempPos = hit.point;
-                GameObject currObj = ListObjectsForPlace[numbObj - 1];
-                SetToSurface(currObj, tempPos);
-                currObj.GetComponent<Rigidbody>().isKinematic = true;
-                numbObj--;
-
+                if (!hit.collider.CompareTag("ObjectForSet"))
+                {
+                    GameObject currObj = ListObjectsForPlace[numbObj - 1];
+                    SetToSurface(currObj, hit.point);
+                    currObj.GetComponent<Rigidbody>().isKinematic = true;
+                    if (!DoesIntersectsBoundsTwoObj(currObj, ListPlacedObjects))
+                        numbObj--;
+                    else
+                        currObj.transform.position += Vector3.up * 11;
+                }
             }
-
-            //if (!DoesIntersectsBoundsTwoObj(ListObjectsForPlace[numbObj - 1], ListPlacedObjects))
         }
     }
     void SetToSurface(GameObject objForPlace, Vector3 positionForPlaceObject)
     {
         Bounds currObjBounds = objForPlace.GetComponent<Renderer>().bounds;
 
-        float tY = currObjBounds.size.y / 2;       // the profile of the object is taken into account in brackets
-        positionForPlaceObject += Vector3.up * tY;
+        positionForPlaceObject += Vector3.up * (currObjBounds.size.y / 2);      // the profile.y of the object is taken into account in brackets
         objForPlace.transform.position = positionForPlaceObject;
     }
 
@@ -97,7 +105,6 @@ public class ItemPlacer : MonoBehaviour
                 return true;
 
         ListIntersectionWithThese.Add(currObj);
-        currObj.GetComponent<Rigidbody>().isKinematic = true;
         return false;
     }
     Vector3 GetCoordinate(Bounds bounds)
@@ -117,11 +124,10 @@ public class ItemPlacer : MonoBehaviour
     List<GameObject> GetListOfObjectForPlace(GameObject[] A_itemPrefabs)
     // this method should be another, depends from rules
     {
-        //List<GameObject> tempObjectList = new List<GameObject>();
         List<GameObject> tempObjectList = new List<GameObject>();
 
         for (int it = 0; it < A_itemPrefabs.Length; it++)
-            tempObjectList.Add(Instantiate(A_itemPrefabs[it], Vector3.up * (15 + it*5), Quaternion.identity));
+            tempObjectList.Add(Instantiate(A_itemPrefabs[it], Vector3.up * (15 + it*2), Quaternion.identity));
         return tempObjectList;
     }
     void OnDrawGizmos()
